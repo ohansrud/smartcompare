b4f3dddea2cbc9bed0d6ab78cfd85908b1a6edd5
ohansrud <ohansrud@gmail.com>
2017-03-17 10:02:39 +0100

    Added code

---
+++ src/Comparer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic;
+using System.Reflection;
+
+namespace Noc.App_Helpers
+{
+    public class CompareResult<T>
+    {
+        public List<T> Added { get; set; }
+        public List<T> Deleted { get; set; }
+        public List<T> Altered { get; set; }
+        public List<T> Unaltered { get; set; }
+
+        public CompareResult()
+        {
+            this.Altered = new List<T>();
+            this.Unaltered = new List<T>();
+            this.Deleted = new List<T>();
+            this.Added = new List<T>();
+        }
+    }
+
+    public static class Comparer
+    {
+        public static bool PublicInstancePropertiesEqual<T>(this T self, T to, params string[] ignore) where T : class
+        {
+            if ((self != null) && (to != null))
+            {
+                var type = typeof(T);
+                var ignoreList = new List<string>(ignore);
+                var unequalProperties =
+                    from pi in type.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                    where !ignoreList.Contains(pi.Name)
+                    let selfValue = type.GetProperty(pi.Name).GetValue(self, null)
+                    let toValue = type.GetProperty(pi.Name).GetValue(to, null)
+                    where (selfValue != toValue) && ((selfValue == null) || !selfValue.Equals(toValue))
+                    select selfValue;
+                return !unequalProperties.Any();
+            }
+            return self == to;
+        }
+
+
+        public static CompareResult<T> ListComparer<T>(List<T> oldList, List<T> newList, string key, string[] ignore)
+        {
+            var res = new CompareResult<T>();
+            foreach (var to in oldList)
+            {
+                var h = Convert.ToInt32(to.GetType().GetProperty(key).GetValue(to, null));
+
+                var pred = key + " == " + h;
+
+                var self = newList.Where(pred).FirstOrDefault();
+                if(self == null)
+                {
+                    res.Deleted.Add(to);
+                }
+            }
+
+            foreach (var to in newList.Except(res.Added).Except(res.Deleted))
+            {
+                var h = Convert.ToInt32(to.GetType().GetProperty(key).GetValue(to, null));
+
+                var pred = key + " == " + h;
+
+                var self = oldList.Where(pred).FirstOrDefault();
+                if (self == null)
+                {
+                    res.Added.Add(to);
+                }
+                else if ((self != null) && (to != null))
+                {
+                    var type = typeof(T);
+                    var ignoreList = new List<string>(ignore);
+                    var unequalProperties =
+                        from pi in type.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                        where !ignoreList.Contains(pi.Name)
+                        let selfValue = type.GetProperty(pi.Name).GetValue(self, null)
+                        let toValue = type.GetProperty(pi.Name).GetValue(to, null)
+                        where (selfValue != toValue) && ((selfValue == null) || !selfValue.Equals(toValue))
+                        select selfValue;
+                    if (unequalProperties.Any())
+                        res.Altered.Add(to);
+                    else
+                        res.Unaltered.Add(to);
+                }
+            }
+
+            return res;
+        }
+    }
+}
---
+++ test/UnitTest_Comparer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Noc.Tests
+{
+    public class TestObject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Modified { get; set; }
+    }
+
+    [TestClass]
+    public class UnitTest_Comparer
+    {
+        List<TestObject> list1 = new List<TestObject>
+            {
+                new TestObject {Id=1, Modified=new DateTime(2012,1,1,10,0,0), Name="SomeName" }
+            };
+
+        List<TestObject> list2 = new List<TestObject>
+            {
+                new TestObject {Id=1, Modified=new DateTime(2012,1,1,10,0,0), Name="SomeName"
+            },
+            {
+                new TestObject {Id=2, Modified=new DateTime(2012,1,1,10,0,0), Name="SomeName" }
+            }
+        };
+        List<TestObject> list3 = new List<TestObject>
+            {
+                new TestObject {Id=1, Modified=new DateTime(2015,1,1,10,0,0), Name="SomeName" }
+            };
+
+            
+        [TestMethod]
+        public void Test_Unaltered()
+        {
+            var c = Comparer.ListComparer<TestObject>(list1, list1, "Id", new List<string>().ToArray());
+
+            Assert.IsTrue(c.Unaltered.Count == 1);
+        }
+
+        [TestMethod]
+        public void Test_Add_one_object()
+        {
+            var c = Comparer.ListComparer<TestObject>(list1, list2, "Id", new List<string>().ToArray());
+
+            Assert.IsTrue(c.Added.Count == 1);
+        }
+
+        [TestMethod]
+        public void Test_Remove_one_object()
+        {
+            var c = Comparer.ListComparer<TestObject>(list2, list1, "Id", new List<string>().ToArray());
+
+            Assert.IsTrue(c.Deleted.Count == 1);
+        }
+        
+        [TestMethod]
+        public void Test_Ignore_property()
+        {
+            var c = Comparer.ListComparer<TestObject>(list2, list1, "Id", ["Modified"]);
+
+            Assert.IsTrue(c.Unaltered.Count == 1);
+        }
+    }
+}
